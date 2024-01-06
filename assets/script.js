9ecbbe376b763e0204edd757f3aa9ef57a30fb88
eugenio socha <eugenio.socha2004@gmail.com>
2024-01-05 21:31:55 -0400

    Testando funcionalidades e upgrades

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,43 @@
 let caixa_de_texto = document.querySelector(".text-box");
-let botao = document.querySelector("#botao-9");
+let numeros = [];
 
-function encontraBotao () {}
-
-function adicionaNumero (){
-    caixa_de_texto.value += 9; 
+function adicionaNumero (valor){
+    if(valor == '+'){
+        numeros.push(caixa_de_texto.value);
+        caixa_de_texto.value = '';
+    }
+    else{
+        caixa_de_texto.value += valor;
+    }
+    console.log(numeros)
 }
 
-botao.addEventListener("click", adicionaNumero);
+function limpaCampo(){
+    caixa_de_texto.value = '';
+    while(numeros.length){
+        numeros.pop();
+    }
+}
+
+function realizaOperacao () {
+    let resultado = 0;
+
+    if(caixa_de_texto.value){
+        resultado += parseFloat(caixa_de_texto.value);
+    }
+
+    else if (caixa_de_texto.value = '' || numeros.length == 0){
+        window.alert("Insira valores para realizar a operação")
+    }
+    else if (numeros.length){
+        for(let i=0; i < numeros.length; i++){
+            resultado += parseFloat(numeros[i]); 
+        }
+        window.alert(`A soma é ${resultado}`);
+    }
+    
+    while(numeros.length){ // Limpa o array
+        numeros.pop();
+    }
+}
+
